241d9ab4b6950ed2eea76ca13378cfdf2a11103b
alokjha96 <55464212+alokjha96@users.noreply.github.com>
2023-07-16 16:24:48 +0530

    Add files via upload

---
+++ qmoney/src/main/java/com/crio/warmup/stock/tingo_curl.sh
@@ -0,0 +1,5 @@
+curl -l --http2 https://gitlab.crio.do/crio_bytes/me_linux1/-/blob/master/apache_logs.txt
+curl-O https://gitlab.crio.do/crio_bytes/me_linux1/-/blob/master/apache_logs.txt\
+curl -x 192.168.44.1:8888 https://gitlab.crio.do/crio_bytes/me_linux1/-/blob/master/apache_logs.txt\
+curl -H "Accept application/vnd.github.v3+json" https://api.github.com/rate_limit
+curl -H "Accept application/vnd.github.v3+json" https://api.github.com/user
